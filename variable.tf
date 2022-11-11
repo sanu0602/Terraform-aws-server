d4bbb997046fc264ba3dc21f57a5b3814ccc8674
kumar shanu <kshanu0602@gmail.com>
2022-11-11 12:00:31 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "image" {
+  type = string
+  default = "ami-0603cbe34fd08cb81"
+  description = "Enter your APP Image to be deployed"
+}
+
+variable "hw" {
+  type = string
+  default = "t2.nano"
+  description = "Enter your hardware model"
+}
+
+variable "name" {
+  type = string
+  default = "kumar-server-1"
+  description = "Enter your server name"
+}
